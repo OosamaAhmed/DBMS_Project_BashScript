3f514eb34b110df4adfd1b36d1227d38777594fa
Osama Ahmed <73891403+OsamaAhmed187@users.noreply.github.com>
2023-02-04 19:15:53 +0200

    v.7

--- dropDB.sh
+++ dropDB.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+
+
+
 read -p "Enter name of database you want to delete it >>  " database_Name
+
+while [ -z $database_Name ]
+do
+read -p "Enter name of database you want to delete it >>  " database_Name
+
+done
+
+
 if [[ -d DB/$database_Name ]]
 then
 rm -r ./DB/$database_Name
 echo " Database removed Successfully !"
+ . Main_Menu.sh
+
 else
 echo "There was no database in this name enter name of database again"
  . Main_Menu.sh
